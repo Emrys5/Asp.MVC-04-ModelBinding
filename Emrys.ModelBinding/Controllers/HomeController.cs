66019a3fe2e56dee1b9790554716c2732c43bca9
Emrys5 <9097206@qq.com>
2017-05-31 00:35:39 +0800

    添加form

--- Emrys.ModelBinding/Controllers/HomeController.cs
+++ Emrys.ModelBinding/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Emrys.ModelBinding.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,16 @@ namespace Emrys.ModelBinding.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserInfo user)
+        {
+            return Json(user);
         }
 
     }
---
+++ Emrys.ModelBinding/Models/UserInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Emrys.ModelBinding.Models
+{
+public class UserInfo
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string[] Bobbys { get; set; }
+    public Company Company { get; set; }
+    public Star[] Star { get; set; }
+}
+public class Company
+{
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string[] Tel { get; set; }
+}
+
+public class Star
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Movie { get; set; }
+}
+}
